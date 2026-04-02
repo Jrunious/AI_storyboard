504054e33480189cdf018e700188e9fccc4116ea
Jrunious <jrunious@gmail.com>
2026-04-02 14:48:13 +0200

    Create layout.js

---
+++ app/app/layout.js
@@ -0,0 +1,8 @@
+export const metadata = { title: 'Storyboard Builder' }
+export default function RootLayout({ children }) {
+  return (
+    <html lang="nl">
+      <body>{children}</body>
+    </html>
+  )
+}
